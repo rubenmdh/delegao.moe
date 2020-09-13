1408fab924f5a5d259087a8ca000d9fde43cdfde
Delegao <rubenmdh@gmail.com>
2020-09-13 18:00:48 +0200

    Fix session check

--- functions/modify-post-db.php
+++ functions/modify-post-db.php
@@ -2,9 +2,12 @@
 
 require_once "../config.php";
 
+// Initialize the session
+session_start();
+
 // Check if the user is logged in, if not then redirect them to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: ../login.php");
     exit;
 }
 
